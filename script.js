767bc574ca9fe1e7b58b957d6792b76c20e0a385
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-09-28 18:09:30 -0700

    update the javascript file

--- script.js
+++ script.js
@@ -7,15 +7,40 @@
 //whenever the user --- wins/ties/losses - the count is added to the respective vars.
 //window.alert() - to alert the users about the status.
 //window.confirm() - to ask if the user wants to play again.
-
 var win = 0;
 var tie = 0;
 var loss = 0;
-
-var userChoice = prompt("Pick one :P,R,S");
-userChoice = userChoice.toUpperCase();
-var computerOptions = ["P", "R", "S"];
-var computerChoice =
-  computerOptions[Math.floor(Math.random() * computerOptions.length)];
-console.log(userChoice);
-console.log(computerChoice);
+do {
+  var userChoice = prompt("Pick one :P,R,S");
+  userChoice = userChoice.toUpperCase();
+  var computerOptions = ["P", "R", "S"];
+  var computerChoice =
+    computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  console.log(userChoice);
+  console.log(computerChoice);
+  if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    win++;
+    console.log("You won", win, "times");
+    alert("You won" + " " + win + " " + "times");
+  } else if (
+    (userChoice === "R" && computerChoice === "P") ||
+    (userChoice === "P" && computerChoice === "S") ||
+    (userChoice === "S" && computerChoice === "R")
+  ) {
+    loss++;
+    console.log("You lost", loss, "times");
+    alert("You lost" + loss + "times");
+  } else if (
+    (userChoice === "P" && computerChoice === "P") ||
+    (userChoice === "R" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "S")
+  ) {
+    tie + 1;
+    console.log("You tied", tie, "times");
+    alert("You tied" + tie + "times");
+  }
+} while (confirm("You want to play again?"));
